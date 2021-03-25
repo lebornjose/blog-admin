319cf328e0ac3b34f079dfcc05c5816ce12d8b90
lebornjose <lebornjose@gmail.com>
2021-03-25 22:37:56 +0800

    添加sass 和header

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,36 @@
 <template>
   <div class="container">
-    <el-row>
-      <el-col :span="12">
-        <h3>super</h3>
-        <el-menu>
-          <el-menu-item index="1">
-            <i class="el-icon-menu"></i>
-            <template #title>首页</template>
-          </el-menu-item>
-        </el-menu>
-      </el-col>
-    </el-row>
+    <header class="next-shell-header">
+
+    </header>
+    <left-bar></left-bar>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import leftBar from './components/mods/left-bar.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: {
+    leftBar
+  }
 })
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+}
+.next-shell-header{
+  color: #fff;
+  height: 52px;
+  background: #18263c;
+  border-bottom: 1px solid #eee;
+  -webkit-box-shadow: 0 1px 3px 0 rgb(0 0 0 / 12%);
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 12%);
+  padding: 0 16px;
 }
 </style>
---
+++ src/components/mods/left-bar.vue
@@ -0,0 +1,43 @@
+<template>
+<el-row>
+  <el-col :span="3">
+    <el-menu>
+      <el-menu-item index="1">
+        <i class="el-icon-menu"></i>
+        <template #title>首页</template>
+      </el-menu-item>
+      <el-submenu index="2">
+        <template #title>
+          <i class="el-icon-tickets"></i>
+          <span>文章管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="2-1">文章列表</el-menu-item>
+          <el-menu-item index="2-2">文章分类</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="3">
+        <template #title>
+          <i class="el-icon-setting"></i>
+          <span>功能管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="3-1">友情链接</el-menu-item>
+          <el-menu-item index="3-2">应用推荐</el-menu-item>
+          <el-menu-item index="3-3">评理管理</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </el-col>
+</el-row>
+</template>
+
+<script>
+  export default {
+    name: "left-bar.vue"
+  }
+</script>
+
+<style scoped>
+
+</style>
